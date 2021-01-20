c7049bb4077c81be0a00fedd48504dce22840a9c
Preetinder Singh <preetindersingh072@gmail.com>
2021-01-20 17:44:56 +0530

    styled page 404

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../assets/css/page404.css';
 
 // defining and exporting the Page404 function
 
 function Page404() {
-  return <div>404: Nothing found here</div>;
+  return <div id="page-404-container">404: Nothing found here</div>;
 }
 
 export default Page404;
