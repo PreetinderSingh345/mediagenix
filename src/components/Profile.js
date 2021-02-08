254e9255ab6e65581cb06dccb93363a8ffb6eb5d
Preetinder Singh <preetindersingh072@gmail.com>
2021-02-08 10:53:04 +0530

    added basic functionality to the profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,158 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import '../assets/css/profile.css';
 
-// defining and exporting the Profile class
+// defining the Profile class
 
 class Profile extends React.Component {
+  // defining the constructor function
+
+  constructor(props) {
+    super(props);
+
+    // defining the state object
+
+    this.state = {
+      name: props.auth.user.name,
+      password: '',
+      confirmPassword: '',
+      editMode: false,
+    };
+  }
+
+  // handle change function to handle the change in state
+
+  handleChange = (fieldName, value) => {
+    this.setState({
+      [fieldName]: value,
+    });
+  };
+
   render() {
+    // getting the data from props and state
+
+    const { user } = this.props.auth;
+    const { editMode } = this.state;
+
     return (
-      <div>
-        <div>Profile</div>
+      //   {/* link to the settings page */}
 
-        {/* link to the settings page */}
+      //   <Link to="/settings">Settings</Link>
 
-        <Link to="/settings">Settings</Link>
+      // profile container
+
+      <div id="profile-container">
+        {/* profile */}
+
+        <div id="profile">
+          {/* profile image container containing the profile image */}
+
+          <div id="profile-img-container">
+            <img
+              id="profile-img"
+              src="https://images.unsplash.com/photo-1580929211634-0e8f1adae279?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8c2lraHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+              alt="user-dp"
+            />
+          </div>
+
+          {/* email(immutable) field container */}
+
+          <div className="fields-container">
+            <div className="field-label">Email</div>
+
+            <div className="field-value">{user.email}</div>
+          </div>
+
+          {/* name field container(showing input to change the name in edit mode) */}
+
+          <div className="fields-container">
+            <div className="field-label">Name</div>
+
+            {editMode ? (
+              <input
+                type="text"
+                onChange={(event) =>
+                  this.handleChange('name', event.target.value)
+                }
+                value={this.state.name}
+              />
+            ) : (
+              <div className="field-value">{user.name}</div>
+            )}
+          </div>
+
+          {/* showing options to change the password in edit mode */}
+
+          {editMode && (
+            <div className="field">
+              <div className="field-label">New password</div>
+
+              <input
+                type="password"
+                onChange={(event) =>
+                  this.handleChange('password', event.target.value)
+                }
+                value={this.state.password}
+              />
+            </div>
+          )}
+
+          {editMode && (
+            <div className="field">
+              <div className="field-label">Confirm Password</div>
+
+              <input
+                type="password"
+                onChange={(event) =>
+                  this.handleChange('confirmPassword', event.target.value)
+                }
+                value={this.state.confirmPassword}
+              />
+            </div>
+          )}
+
+          {/* showing button(s) depending on whether the user is in edit mode or not */}
+
+          <div id="buttons-container">
+            {editMode ? (
+              <button
+                className="button"
+                onClick={() => this.handleChange('editMode', false)}
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                className="button"
+                onClick={() => this.handleChange('editMode', true)}
+              >
+                Edit Profile
+              </button>
+            )}
+
+            {editMode && (
+              <button
+                className="button"
+                onClick={() => this.handleChange('editMode', false)}
+              >
+                Go back
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+// defining the map state to props function
+
+function mapStateToProps({ auth }) {
+  return {
+    auth,
+  };
+}
+
+// exporting the connected Profile component
+
+export default connect(mapStateToProps)(Profile);
